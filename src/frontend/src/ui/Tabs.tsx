47e27fdc3c12be3cc677c403aa1bcbc1fcf70bb9
Alex TYRODE <alex@tyrode.dev>
2025-04-28 23:12:15 +0000

    feat: add Tabs component styling and functionality
    - Introduced a new Tabs.scss file for styling the Tabs component.
    - Updated Tabs.tsx to implement a tabs bar with a new button for creating a new pad, enhancing user interaction within the Excalidraw interface.

--- src/frontend/src/ui/Tabs.tsx
+++ src/frontend/src/ui/Tabs.tsx
@@ -1,24 +1,9 @@
 import React from "react";
 
 import type { ExcalidrawImperativeAPI } from "@atyrode/excalidraw/types";
-import { Button, MIME_TYPES } from "@atyrode/excalidraw";
-
-const COMMENT_SVG = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="feather feather-message-circle"
-  >
-    <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
-  </svg>
-);
+import { Stack, Button, Section, Tooltip } from "@atyrode/excalidraw";
+import { FilePlus2 } from "lucide-react";
+import "./Tabs.scss";
 
 interface TabsProps {
   excalidrawAPI: ExcalidrawImperativeAPI;
@@ -30,14 +15,28 @@ const Tabs: React.FC<TabsProps> = ({
   excalidrawAPI: ExcalidrawImperativeAPI;
 }) => {
 
-  return (
-    <>
-      <Button
-        onSelect={() => alert("General Kenobi!")}
-      >
-        Hit me
-      </Button>
-    </>
+    const appState = excalidrawAPI.getAppState();
+    return (
+        <div className="tabs-bar">
+            <Stack.Col gap={2}>
+                <Section heading="canvasActions">
+                    {!appState.viewModeEnabled && (
+                        <div className="new-tab-button-container">
+                            <Tooltip label="New pad" children={
+                                <Button
+                                    onSelect={() => {}}
+                                    children={
+                                        <div className="ToolIcon__icon">
+                                            <FilePlus2 />
+                                        </div>}
+                                />
+                            }>
+                            </Tooltip>
+                        </div>
+                    )}
+                </Section>
+            </Stack.Col>
+        </div>
   );
 };
 
