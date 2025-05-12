f05c190ba3cddac63379a3cc3cfcff8d1bc08693
Alex TYRODE <alex@tyrode.dev>
2025-05-12 15:43:17 +0000

    chore: update @atyrode/excalidraw dependency to version 0.18.0-12
    - Updated the version of @atyrode/excalidraw in package.json and yarn.lock to ensure compatibility with the latest features and fixes.
    - Adjusted the integrity hash in yarn.lock to reflect the new version.

--- src/frontend/src/utils/canvasUtils.ts
+++ src/frontend/src/utils/canvasUtils.ts
@@ -47,6 +47,9 @@ export function normalizeCanvasData(data: any) {
   // Reset collaborators (https://github.com/excalidraw/excalidraw/issues/8637)
   appState.collaborators = new Map();
   
+  // Support new appState key default value (https://github.com/excalidraw/excalidraw/commit/a30e1b25c60a9c5c6f049daada0443df874a5266#diff-b7eb4d88c1bc5b4756a01281478e2105db6502e96c2a4b855496c508cef05397L124-R124)
+  appState.searchMatches = null;
+
   return { ...data, appState };
 }
 
