5fe16bba121ee2c1a83fb777a4a8db62ea990746
Alex TYRODE <alex@tyrode.dev>
2025-05-13 11:03:51 +0000

    removed unused imports

--- src/backend/database/__init__.py
+++ src/backend/database/__init__.py
@@ -7,13 +7,9 @@ This module provides access to all database components used in the application.
 from .database import (
     init_db, 
     get_session, 
-    get_user_repository, 
-    get_pad_repository, 
 )
 
 __all__ = [
     'init_db',
     'get_session',
-    'get_user_repository',
-    'get_pad_repository',
 ]
